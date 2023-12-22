040b82c5f4ac3c935c2c25596f5db82ef72b8e05
Sunflair0 <62845098+Sunflair0@users.noreply.github.com>
2023-12-22 03:48:17 -0600

    fluted glass

---
+++ mi_script.js
@@ -0,0 +1,34 @@
+var button = $('.submit-btn');
+ var message = $('.message');
+
+ button.on('click', changeTab)
+ 
+ function changeTab(event){
+   event.preventDefault();
+   var email = $('.email-input').val();
+   message.text(`Thank you for submitting your email ${email}. We will contact you shortly on ways to fight climate change!`);
+ }
+
+ //$(document).ready(function() {
+ // $('.first-submit').attr('disabled', 'disabled');
+ // var numFields=0;
+ // $('#email[type=text').on('input', function() {
+ //   if ($(this).val() !== '') {
+ //     numFields++;
+ //   }
+ //   if (numFields === 3) {
+ //     $('.first-submit').removeAttr("disabled");
+ //   }
+ // });
+ // console.log('Thank you so much. Have a nice day.');
+ //});
+
+//I was inspired from Sandra Cheah's projects. I think the code will come out.
+
+//Otherwise, I'm glad for looking at references to understand how to code properly.
+
+//let modelElem = createModelElement('model.glb', -100, -100, -100);
+
+//sceneElem.appendChild(modelElem);
+
+/*Code would not properly work fore me*/
