778333d9b93e48967709914ffbb5ae9f59d9dab1
Sunflair0 <62845098+Sunflair0@users.noreply.github.com>
2023-12-28 03:51:32 -0600

    back button for #8

--- mi_script.js
+++ mi_script.js
@@ -1,27 +1,27 @@
 var button = $('.submit-btn');
- var message = $('.message');
+var message = $('.message');
 
- button.on('click', changeTab)
- 
- function changeTab(event){
-   event.preventDefault();
-   var email = $('.email-input').val();
-   message.text(`Thank you for submitting your email ${email}. We will contact you shortly on ways to fight climate change!`);
- }
+button.on('click', changeTab)
 
- //$(document).ready(function() {
- // $('.first-submit').attr('disabled', 'disabled');
- // var numFields=0;
- // $('#email[type=text').on('input', function() {
- //   if ($(this).val() !== '') {
- //     numFields++;
- //   }
- //   if (numFields === 3) {
- //     $('.first-submit').removeAttr("disabled");
- //   }
- // });
- // console.log('Thank you so much. Have a nice day.');
- //});
+function changeTab(event) {
+  event.preventDefault();
+  var email = $('.email-input').val();
+  message.text(`Thank you for submitting your email ${email}. We will contact you shortly on ways to fight climate change!`);
+}
+
+//$(document).ready(function() {
+// $('.first-submit').attr('disabled', 'disabled');
+// var numFields=0;
+// $('#email[type=text').on('input', function() {
+//   if ($(this).val() !== '') {
+//     numFields++;
+//   }
+//   if (numFields === 3) {
+//     $('.first-submit').removeAttr("disabled");
+//   }
+// });
+// console.log('Thank you so much. Have a nice day.');
+//});
 
 //I was inspired from Sandra Cheah's projects. I think the code will come out.
 
